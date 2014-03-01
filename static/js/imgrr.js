41a8cb95c7ffa94ca1b610dd15caa3d6fe2662ff
Kangting Yu <kangting@Kangtings-MacBook-Pro.local>
2014-03-01 15:42:56 -0800

    imgrr completed

--- static/js/imgrr.js
+++ static/js/imgrr.js
@@ -1,13 +1,26 @@
 // Problem 2 (Peekaboo) ------------------------------------------------------
-// WRITE CODE HERE
+$('#toggle_img').click(function() {
+	if ($('#toggle_img').text() === 'Go Away!'){
+		$('#main_img').hide();
+		$('#toggle_img').text('Come Back!');
+	} else {
+		$('#main_img').show();
+		$('#toggle_img').text('Go Away!');
+	};
+});
 
 // Problem 3 (Swap Em) -----------------------------------------------
-// WRITE CODE HERE
-
+$('#change_img').click(function(){
+	$('#main_img').attr('src','/static/img/' + $('#new_img_file').val());
+});
 // Problem 4 (Find the Source) -------------------------------------------------
 $('.clickable').click(function() {
-  // WRITE CODE HERE
+  alert($(this).attr('src'));
 });
 
 // Problem 5 (Imgrr) -------------------------------------------------
-// WRITE CODE HERE
+$('.clickable2').click(function() {
+	var path = $(this).attr('src');
+	$(this).attr('src',$('#main_img2').attr('src'));
+	$('#main_img2').attr('src',path);
+});
