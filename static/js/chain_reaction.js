cdc06946345b5fb03299f9800f849aa6059c1f47
Kangting Yu <kangting@Kangtings-MacBook-Pro.local>
2014-04-28 17:19:23 -0700

    Lab 8 DONE

--- static/js/chain_reaction.js
+++ static/js/chain_reaction.js
@@ -6,10 +6,12 @@ $(document).ready(function() {
   var height = canvas.height;
   var color = ['red','green','yellow','blue','black'];
 
-  var numBalls = 10
+  var reactions =[];
+  var numBalls = 10;
   var balls = [];
+  var radius = 30;
   for ( var i = 0; i<numBalls;i++){
-    var bi = {x:Math.random()*canvas.width, y:Math.random()*canvas.height, radius:10, vx: (Math.random()-0.5)*20, vy: (Math.random()-0.5)*20, color: color[Math.floor(Math.random()*5)]};
+    var bi = {x:Math.random()*canvas.width, y:Math.random()*canvas.height, radius:radius, vx: (Math.random()-0.5)*10, vy: (Math.random()-0.5)*10, color: color[Math.floor(Math.random()*5)]};
     balls.push(bi)
   }
   var drawCircle = function(x, y, radius, color) {
@@ -40,7 +42,35 @@ $(document).ready(function() {
     drawCircle(balls[i].x,balls[i].y,balls[i].radius,balls[i].color);
     balls[i].x += balls[i].vx;
     balls[i].y += balls[i].vy;
-  };
+    };
+    for (var i =0;i<reactions.length;i++){
+      drawCircle(reactions[i].x,reactions[i].y,reactions[i].radius,reactions[i].color);
+    }
+    for (var i=0;i<reactions.length;i++){
+      if(reactions[i].radius<30){
+        reactions[i].radius++;
+      }
+    }
+    for (var i = 0; i < balls.length; i++) {
+      var collided = "False" ;
+      for (var j = 0; j < reactions.length; j++) {
+        xdiff= balls[i].x-reactions[j].x;
+        ydiff= balls[i].y-reactions[j].y;
+        dist = Math.sqrt(xdiff*xdiff+ydiff*ydiff);
+        if(dist<(balls[i].radius+reactions[j].radius)){
+          collided = "True";
+        }
+      }
+      if (collided == "True"){
+        balls[i].raidus=1;
+        var x = balls[i].x;
+        var y = balls[i].y;
+        var b = {x: x, y: y, radius:1, color: color[Math.floor(Math.random()*5)]};
+        reactions.push(b);
+        balls.splice(i,1);
+        i--;
+      }
+    }    
   requestAnimationFrame(updateGame);
   };
 
@@ -49,8 +79,8 @@ $(document).ready(function() {
     // Find the mouse x and y relative to the top-left corner of the canvas
     var x = e.pageX - $(this).offset().left;
     var y = e.pageY - $(this).offset().top;
-    var b = {x: x, y: y, radius:10, vx: (Math.random()-0.5)*20, vy: (Math.random()-0.5)*20, color: color[Math.floor(Math.random()*5)]};
-    balls.push(b)
+    var b = {x: x, y: y, radius:1, color: color[Math.floor(Math.random()*5)]};
+    reactions.push(b)
   });
 
   updateGame();
